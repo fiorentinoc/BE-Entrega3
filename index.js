9f952ba9c2877d6a99c6bb97b8408ce6a13edef5
Christian Fiorentino <cfiorentino@gmail.com>
2022-12-02 00:41:15 -0300

    Clase + productos.txt

--- index.js
+++ index.js
@@ -1,12 +1,105 @@
 const express = require('express')
 const app = express()
 
-/* app.get('/', function(req, res) {
-    res.send('Hola mundo')
-}) */
+const fs = require ('fs')
+
+
+/* CLASE Contenedor */
+
+class Contenedor {
+
+  constructor( file ) {
+      this.file = file
+  }
+
+  
+  async getAll() {
+    try{
+      const objects = await fs.promises.readFile( this.file, 'utf-8')
+      return JSON.parse(objects)
+
+    } catch(err) {
+      console.log(`Se produjo el siguiente error: ${err}`)
+    }
+  }
+ 
+  
+  async saveFile ( file, objects ) {
+    try {
+      await fs.promises.writeFile(
+        file, JSON.stringify( objects, null, 2 )
+        )
+    } catch(err) {
+      console.log(`Se produjo el siguiente error: ${err}`)
+    }
+  }
+
+
+  async save( object ) {
+    const objects = await this.getAll()
+    try{
+        let idNew
+        objects.length === 0 
+          ? idNew = 1
+          : idNew = objects[ objects.length - 1 ].id + 1
+        
+        const objectNew = { id: idNew, ...object }       
+        objects.push(objectNew)        
+        await this.saveFile(this.file, objects)
+        return idNew
+
+    } catch(err) {
+      console.log(`Se produjo el siguiente error: ${err}`)
+    }
+  }
+
+
+  async getById( id ) {
+    const objects = await this.getAll()
+    try {
+      const object = objects.find( ele => ele.id === id)
+      return object ? object : null
+
+    } catch(err) {
+      console.log(`Se produjo el siguiente error: ${err}`)
+    }
+  }
+
+
+  async deleteById( id ) {
+    let objects = await this.getAll()
+    
+    try {
+      objects = objects.filter( ele => ele.id != id )
+      await this.saveFile( this.file, objects)
+    
+    } catch(err) {
+      console.log(`Se produjo el siguiente error: ${err}`)
+    }
+  }
+
+
+  async deleteAll() {
+    await this.saveFile(this.file, [])
+  }
+
+}
+/* ******************Fin CLASE Contenedor***************** */
+
+const productos = new Contenedor('productos.txt')
+
 
 app.get('/productos', (req, res) => {
-    res.send('productos')
+    const obtenerProductos = async() => {
+        try {
+            let arrayProductos = await productos.getAll()
+            res.send(arrayProductos)    
+        } 
+        catch (err) {
+            console.log(err)
+        }
+    }
+    obtenerProductos()
 })
 
 app.get('/productoRandom', (req, res) => {
